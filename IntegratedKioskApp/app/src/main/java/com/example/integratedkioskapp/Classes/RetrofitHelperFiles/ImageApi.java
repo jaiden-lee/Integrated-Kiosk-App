f159f53ada0c2cae7f9977d25ec542bf87f8377a
Jaiden Lee <jaidenwlee@gmail.com>
2022-12-02 10:52:59 -0500

    Created Function to Post Multiple Images to FaceRec Server

---
+++ IntegratedKioskApp/app/src/main/java/com/example/integratedkioskapp/Classes/RetrofitHelperFiles/ImageApi.java
@@ -0,0 +1,14 @@
+package com.example.integratedkioskapp.Classes.RetrofitHelperFiles;
+
+import okhttp3.MultipartBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+
+public interface ImageApi {
+    @Multipart
+    @POST("post_images/") // this is the part of the URL after the base URL
+    public Call<ResponseBody> postImagesForFaceRec (@Part MultipartBody.Part[] images);
+}
