a96b2be689ea4b456ef46d3854a2831596d5722e
Jaiden Lee <jaidenwlee@gmail.com>
2022-12-02 10:33:02 -0500

    Setup Retrofit

--- IntegratedKioskApp/app/src/main/java/com/example/integratedkioskapp/Classes/ServerCommunication.java
+++ IntegratedKioskApp/app/src/main/java/com/example/integratedkioskapp/Classes/ServerCommunication.java
@@ -1,5 +1,11 @@
 package com.example.integratedkioskapp.Classes;
 
-public class ServerCommunication {
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
+public class ServerCommunication {
+    public static final String ai_base_url = "http://192.168.86.59:8000/";
 }
