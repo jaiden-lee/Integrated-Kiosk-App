6ec50d036081fd7729ec9c20905a910de4aa45bc
Jaiden Lee <jaidenwlee@gmail.com>
2022-12-05 12:03:48 -0500

    Numpad Start

--- IntegratedKioskApp/app/src/main/java/com/example/integratedkioskapp/Classes/Numpad.java
+++ IntegratedKioskApp/app/src/main/java/com/example/integratedkioskapp/Classes/Numpad.java
@@ -1,4 +1,14 @@
 package com.example.integratedkioskapp.Classes;
 
+import android.app.Activity;
+import android.content.Context;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 public class Numpad {
+    public static Button button1;
+    public static void createClickListeners (Activity context) {
+        button1 = (Button)context.findViewById(R.id.one);
+    }
 }
