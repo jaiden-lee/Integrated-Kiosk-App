56e9eb7fc0add74dcf2040b883fc1a0c3bab2ae7
Jaiden Lee <jaidenwlee@gmail.com>
2022-12-07 10:35:12 -0500

    Numpad Code to Append Numbers to the ID String

--- IntegratedKioskApp/app/src/main/java/com/example/integratedkioskapp/MainActivity.java
+++ IntegratedKioskApp/app/src/main/java/com/example/integratedkioskapp/MainActivity.java
@@ -3,11 +3,20 @@ package com.example.integratedkioskapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.integratedkioskapp.Classes.Numpad;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button[] numpadButtons = {findViewById(R.id.one), findViewById(R.id.two)};
+        TextView idLabel = findViewById(R.id.display_student_id);
+
+        Numpad.createClickListeners(numpadButtons, idLabel);
     }
 }
