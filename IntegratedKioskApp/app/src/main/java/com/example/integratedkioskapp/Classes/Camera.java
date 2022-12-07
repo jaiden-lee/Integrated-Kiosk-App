c00913a4b40904189686e4eadbb6f84918acf137
mtavel10 <113120790+mtavel10@users.noreply.github.com>
2022-12-07 10:47:16 -0500

    maddydec6

--- IntegratedKioskApp/app/src/main/java/com/example/integratedkioskapp/Classes/Camera.java
+++ IntegratedKioskApp/app/src/main/java/com/example/integratedkioskapp/Classes/Camera.java
@@ -1,20 +1,23 @@
 package com.example.integratedkioskapp.Classes;
 import android.view.SurfaceHolder;
-import com.google.android.gms.vision.*;
+
+import com.google.mlkit.vision.camera.CameraSourceConfig;
+import com.google.mlkit.vision.camera.CameraXSource;
 
 public class Camera {
 
-   // private static CameraSource cameraSource;
+    private static CameraXSource cameraSource;
     private static final int REQUEST_CAMERA_PERMISSION = 201;
 
     public static void buildCamera(){
-//        cameraSource = new CameraSource.Builder(this,barcodeDetector)
-//                .setRequestedPreviewSize(1920, 1080)
-//                //1 for facing front and 0 for facing back
-//                .setFacing(0)
-//                .setAutoFocusEnabled(true)
-//                //.setFocusMode(FOCUS_MODE_CONTINUOUS_PICTURE) read the documentation about this
-//                .build();
+
+        cameraSource = new CameraXSource(new CameraSourceConfig.Builder())
+                .setRequestedPreviewSize(1920, 1080)
+                //1 for facing front and 0 for facing back
+                .setFacing(0)
+                .setAutoFocusEnabled(true)
+                //.setFocusMode(FOCUS_MODE_CONTINUOUS_PICTURE) read the documentation about this
+                .build();
 
     }
 
