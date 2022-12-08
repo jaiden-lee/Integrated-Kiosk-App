4a469773b0956b9d44485dc0bf08ea5e8a12c99e
Jaiden Lee <jaidenwlee@gmail.com>
2022-12-08 10:52:32 -0500

    Numpad

--- IntegratedKioskApp/app/src/main/java/com/example/integratedkioskapp/MainActivity.java
+++ IntegratedKioskApp/app/src/main/java/com/example/integratedkioskapp/MainActivity.java
@@ -14,7 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button[] numpadButtons = {findViewById(R.id.one), findViewById(R.id.two)};
+        Button[] numpadButtons = {
+                findViewById(R.id.zero),
+                findViewById(R.id.one),
+                findViewById(R.id.two),
+                findViewById(R.id.three),
+                findViewById(R.id.four),
+                findViewById(R.id.five),
+                findViewById(R.id.six),
+                findViewById(R.id.seven),
+                findViewById(R.id.eight),
+                findViewById(R.id.nine)
+        };
         TextView idLabel = findViewById(R.id.display_student_id);
 
         Numpad.createClickListeners(numpadButtons, idLabel);
