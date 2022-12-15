cfc44d349ced84c6001f370af8ce7274c6a753da
Jaiden Lee <jaidenwlee@gmail.com>
2022-12-15 10:55:28 -0500

    View Binding Stuff

--- IntegratedKioskApp/app/src/main/java/com/example/integratedkioskapp/Classes/Numpad.java
+++ IntegratedKioskApp/app/src/main/java/com/example/integratedkioskapp/Classes/Numpad.java
@@ -2,26 +2,42 @@ package com.example.integratedkioskapp.Classes;
 
 import android.app.Activity;
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.integratedkioskapp.MainActivity;
+import com.example.integratedkioskapp.databinding.ActivityMainBinding;
 
 public class Numpad {
     public static String currentId = "";
     public static int butNum;
-    public static void createClickListeners (Button[] buttons, TextView idLabel) {
-        for (int buttonNum = 0; buttonNum<buttons.length; buttonNum++) {
-            butNum = buttonNum+1;
-            buttons[buttonNum].setOnClickListener (new View.OnClickListener() {
-                int num = butNum;
+    public static void createClickListeners (ActivityMainBinding binding) {
+
+        Button button1 = binding.one;
+        button1.setOnClickListener (new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    currentId+=num;
-                    idLabel.setText(currentId);
+                    currentId+="1";
+                    Log.d("NUMPAD", "onClick: "+currentId);
                 }
             });
-        }
+
+
+//        for (int buttonNum = 0; buttonNum<buttons.length; buttonNum++) {
+//            butNum = buttonNum+1;
+//            buttons[buttonNum].setOnClickListener (new View.OnClickListener() {
+//                int num = butNum;
+//                @Override
+//                public void onClick(View view) {
+//                    currentId+=num;
+//                    idLabel.setText(currentId);
+//                }
+//            });
+//        }
     }
 }
--- IntegratedKioskApp/app/src/main/java/com/example/integratedkioskapp/MainActivity.java
+++ IntegratedKioskApp/app/src/main/java/com/example/integratedkioskapp/MainActivity.java
@@ -3,68 +3,42 @@ package com.example.integratedkioskapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-//camera stuff
-import android.Manifest;
-import android.content.ContentValues;
-import android.content.pm.PackageManager;
-import android.os.Build;
-import android.os.Bundle;
-import android.provider.MediaStore;
-//what is this
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.core.ImageCapture;
-import androidx.camera.video.Recorder;
-import androidx.camera.video.Recording;
-import androidx.camera.video.VideoCapture;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import com.android.example.cameraxapp.databinding.ActivityMainBinding;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import android.widget.Toast;
-import androidx.camera.lifecycle.ProcessCameraProvider;
-import androidx.camera.core.Preview;
-import androidx.camera.core.CameraSelector;
-import android.util.Log;
-import androidx.camera.core.ImageAnalysis;
-import androidx.camera.core.ImageCaptureException;
-import androidx.camera.core.ImageProxy;
-import androidx.camera.video.FallbackStrategy;
-import androidx.camera.video.MediaStoreOutputOptions;
-import androidx.camera.video.Quality;
-import androidx.camera.video.QualitySelector;
-import androidx.camera.video.VideoRecordEvent;
-import androidx.core.content.PermissionChecker;
-import java.nio.ByteBuffer;
-import java.text.SimpleDateFormat;
-import java.util.Locale;
 
 import com.example.integratedkioskapp.Classes.Numpad;
 import com.example.integratedkioskapp.Classes.Camera;
+import com.example.integratedkioskapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button[] numpadButtons = {
-                findViewById(R.id.zero),
-                findViewById(R.id.one),
-                findViewById(R.id.two),
-                findViewById(R.id.three),
-                findViewById(R.id.four),
-                findViewById(R.id.five),
-                findViewById(R.id.six),
-                findViewById(R.id.seven),
-                findViewById(R.id.eight),
-                findViewById(R.id.nine)
-        };
-        TextView idLabel = findViewById(R.id.display_student_id);
+//        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
-        Numpad.createClickListeners(numpadButtons, idLabel);
+
+
+//        Button[] numpadButtons = {
+//                findViewById(R.id.zero),
+//                findViewById(R.id.one),
+//                findViewById(R.id.two),
+//                findViewById(R.id.three),
+//                findViewById(R.id.four),
+//                findViewById(R.id.five),
+//                findViewById(R.id.six),
+//                findViewById(R.id.seven),
+//                findViewById(R.id.eight),
+//                findViewById(R.id.nine)
+//        };
+//        TextView idLabel = findViewById(R.id.display_student_id);
+
+        Numpad.createClickListeners(binding);
     }
 }
