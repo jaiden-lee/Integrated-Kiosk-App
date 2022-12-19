6e93d2f448b2954f88797383fcb7cf7faad03b0b
mtavel10 <113120790+mtavel10@users.noreply.github.com>
2022-12-19 10:52:01 -0500

    camera imports

--- IntegratedKioskApp/app/src/main/java/com/example/integratedkioskapp/Classes/Camera.java
+++ IntegratedKioskApp/app/src/main/java/com/example/integratedkioskapp/Classes/Camera.java
@@ -2,11 +2,43 @@ package com.example.integratedkioskapp.Classes;
 
 import com.example.integratedkioskapp.databinding.ActivityMainBinding;
 
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.video.Recorder;
+import androidx.camera.video.Recording;
+import androidx.camera.video.VideoCapture;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import android.widget.Toast;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.core.Preview;
+import androidx.camera.core.CameraSelector;
+import android.util.Log;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.video.FallbackStrategy;
+import androidx.camera.video.MediaStoreOutputOptions;
+import androidx.camera.video.Quality;
+import androidx.camera.video.QualitySelector;
+import androidx.camera.video.VideoRecordEvent;
+import androidx.core.content.PermissionChecker;
+import java.nio.ByteBuffer;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 public class Camera {
 
-    public static void buildCameraAndDetector(){
 
-
+    public static void checkPermissions(){
     }
 
 
